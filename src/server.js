ea4b9108f0977b4d35d8296aca47d17a9e0425a0
Rodolfo Chan <rodolfochan2910@gmail.com>
2025-07-09 14:49:23 -0600

    Agregada local / para verificación

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Servidor corriendo en http://0.0.0.0:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
+  });
 });
